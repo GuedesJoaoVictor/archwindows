aac6b1835c7ea88a98d63c2d36759fa038a00328
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-02 23:45:18 -0300

    feat: Adicionando minhas configurações do neovim
    Adicionando meus plugins, configurações e keymaps do neovim

---
+++ nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,14 @@
+return {
+  -- "vim-test/vim-test",
+  -- dependencies = {
+    -- "preservim/vimux"
+  -- },
+  -- config = function()
+    -- vim.keymap.set("n", "<leader>t", ":testnearest<cr>", {})
+    -- vim.keymap.set("n", "<leader>t", ":testfile<cr>", {})
+    -- vim.keymap.set("n", "<leader>a", ":testsuite<cr>", {})
+    -- vim.keymap.set("n", "<leader>l", ":testlast<cr>", {})
+    -- vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
+    -- vim.cmd("let test#strategy = 'vimux'")
+  -- end,
+}
